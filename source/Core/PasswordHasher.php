7d1149b1f0793b0c2693b111544e86b97ea9da70
robertblank <robert.blank@oxid-esales.com>
2019-02-13 11:03:11 +0100

    OXDEV-1820 Refactor PasswordHasher to be able to use PasswordHashBcryptService

--- source/Core/PasswordHasher.php
+++ source/Core/PasswordHasher.php
@@ -6,46 +6,81 @@
 
 namespace OxidEsales\EshopCommunity\Core;
 
+use OxidEsales\EshopCommunity\Internal\Password\Exception\PasswordHashException;
+use OxidEsales\EshopCommunity\Internal\Password\Service\PasswordHashBcryptService;
+use OxidEsales\EshopCommunity\Internal\Password\Service\PasswordHashServiceInterface;
+
 /**
  * Hash password together with salt, using set hash algorithm
  */
 class PasswordHasher
 {
     /**
-     * @var \oxHasher
+     * @var \OxidEsales\Eshop\Core\Hasher|PasswordHashServiceInterface
      */
-    private $_ohasher = null;
+    private $passwordHashService;
 
     /**
-     * Gets hasher.
+     * Returns password hash service
      *
-     * @return \OxidEsales\Eshop\Core\Hasher
+     * @return \OxidEsales\Eshop\Core\Hasher|PasswordHashServiceInterface
      */
     protected function _getHasher()
     {
-        return $this->_ohasher;
+        return $this->passwordHashService;
     }
 
     /**
      * Sets dependencies.
      *
-     * @param \OxidEsales\Eshop\Core\Hasher $oHasher hasher.
+     * @param \OxidEsales\Eshop\Core\Hasher|PasswordHashServiceInterface $passwordHashService
      */
-    public function __construct($oHasher)
+    public function __construct($passwordHashService)
     {
-        $this->_ohasher = $oHasher;
+        if (!$passwordHashService instanceof Hasher &&
+            !$passwordHashService instanceof PasswordHashServiceInterface
+        ) {
+            throw new PasswordHashException('Unsupported password hashing service: ' . get_class($passwordHashService));
+        }
+
+        $this->passwordHashService = $passwordHashService;
     }
 
     /**
      * Hash password with a salt.
      *
-     * @param string $sPassword not hashed password.
-     * @param string $sSalt     salt string.
+     * @param string $password not hashed password.
+     * @param string $salt     salt string.
      *
      * @return string
      */
-    public function hash($sPassword, $sSalt)
+    public function hash($password, $salt): string
     {
-        return $this->_getHasher()->hash($sPassword . $sSalt);
+        $passwordHashService = $this->_getHasher();
+
+        if ($passwordHashService instanceof Hasher) {
+            $hash = $passwordHashService->hash($password . $salt);
+        } elseif ($passwordHashService instanceof PasswordHashServiceInterface) {
+            $options = $this->getOptionsForHashService($passwordHashService, $salt);
+
+            $hash = $passwordHashService->hash($password, $options);
+        }
+
+        return $hash;
+    }
+
+    /**
+     * @param PasswordHashServiceInterface $passwordHashService
+     * @param string                       $salt
+     *
+     * @return array
+     */
+    private function getOptionsForHashService(PasswordHashServiceInterface $passwordHashService, string $salt): array
+    {
+        if ($passwordHashService instanceof PasswordHashBcryptService) {
+            $options = ['salt' => $salt];
+        }
+
+        return $options;
     }
 }
